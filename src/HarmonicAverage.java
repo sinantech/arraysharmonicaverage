289a38e610c1d4711c850d781bd0694b33bc9939
sinan can özer <sinancemozer@gmail.com>
2022-12-24 12:41:06 +0300

    first commit

---
+++ src/HarmonicAverage.java
@@ -0,0 +1,16 @@
+public class HarmonicAverage {
+    public static void main(String[] args) {
+        double[] numbers = {1, 2, 3, 4, 5};
+        double[] harmonic = new double[numbers.length];
+
+        double sum = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            harmonic[i] = 1 / numbers[i];
+            sum += harmonic[i];
+        }
+
+        System.out.println("Harmonic Average: " + harmonic.length / sum);
+    }
+}
+
